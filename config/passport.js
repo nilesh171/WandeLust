b28f3ba6a731284a37f07457b33f6bee74bfbe3a
nilesh171 <nileshsonawane0311@gmail.com>
2025-06-29 18:48:54 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ module.exports = function (passport) {
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback'
+      callbackURL: 'https://wandelust-aj3z.onrender.com/auth/google/callback'
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
